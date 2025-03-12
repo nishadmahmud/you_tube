cf75da96e29cc75bb2c0cfb5d5484ef4b1e46715
Nishad <mahmudnishad253@gmail.com>
2025-03-12 21:27:06 +0600

    detail modal and verified

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
-function removeActiveBtn(){
-    const btns = document.getElementsByClassName('active')
-    for(btn of btns){
-        btn.classList.remove('active');
-    }
+function removeActiveBtn() {
+  const btns = document.getElementsByClassName("active");
+  for (btn of btns) {
+    btn.classList.remove("active");
+  }
 }
 
 function loadCata() {
@@ -30,10 +30,11 @@ function loadVideos() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
     .then((data) => {
-        removeActiveBtn();
-        const all = document.getElementById('btn-all');
-        all.classList.add('active');
-        displayVideos(data.videos)})
+      removeActiveBtn();
+      const all = document.getElementById("btn-all");
+      all.classList.add("active");
+      displayVideos(data.videos);
+    })
     .catch((error) => console.error("Error fetching videos:", error));
 }
 
@@ -42,12 +43,39 @@ function loadCataVid(id) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-        const clickBtn = document.getElementById(`btn-${id}`);
-        removeActiveBtn();
-        clickBtn.classList.add("active")
-        displayVideos(data.category);
+      const clickBtn = document.getElementById(`btn-${id}`);
+      removeActiveBtn();
+      clickBtn.classList.add("active");
+      displayVideos(data.category);
     });
 }
+
+function videoDetails(video_id) {
+  console.log(video_id);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${video_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayVideoDetails(data.video);
+    });
+}
+
+const displayVideoDetails = (video) => {
+  document.getElementById("video_details").showModal();
+  const details = document.getElementById("details");
+
+  details.innerHTML = `
+    <div class="card bg-base-100 image-full shadow-sm">
+        <figure>
+            <img src="${video.thumbnail}" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title font-bold">${video.title}</h2>
+            <p>${video.description}</p>
+        </div>
+    </div>
+    `;
+};
 
 function displayVideos(videos) {
   const videoContainer = document.getElementById("video-container");
@@ -61,6 +89,7 @@ function displayVideos(videos) {
     `;
     return;
   }
+
   videos.forEach((video) => {
     const videoCard = document.createElement("div");
 
@@ -72,30 +101,36 @@ function displayVideos(videos) {
       postDate = `${hr}hrs ${mnt}min ago`;
     }
 
-    let veri = "";
-
-    if (video.authors[0].verified) {
-      veri =
-        "https://img.icons8.com/?size=100&id=SRJUuaAShjVD&format=png&color=000000";
-    }
-
     videoCard.innerHTML = `
         <div class="relative">
-            <img src="${video.thumbnail}" class="h-[178px] w-[280px] rounded-sm mb-4 object-cover">
+            <img src="${
+              video.thumbnail
+            }" class="h-[178px] w-[280px] rounded-sm mb-4 object-cover">
             <span class="absolute bottom-2 right-2 rounded-sm p-1 text-sm bg-black text-white">${postDate}</span>
         </div>
         
         <div class="flex flex-start gap-2 pl-2 pb-2">
-            <img src="${video.authors[0].profile_picture}" class="w-9 h-9 rounded-full object-cover">
+            <img src="${
+              video.authors[0].profile_picture
+            }" class="w-9 h-9 rounded-full object-cover">
             <div class="">
                 <h4 class="text-base font-bold">${video.title}</h4>
-                <div class="flex gap-2 items-center">
-                    <h5 class="text-sm font-normal text-[#171717B2]">${video.authors[0].profile_name}</h2>
-                    <img src="${veri}" class="w-4 h-4">
+                <div id="author-info" class="flex gap-2 items-center">
+                    <h5 class="text-sm font-normal text-[rgba(23,23,23,0.7)]">${
+                      video.authors[0].profile_name
+                    }</h2>
+                    ${
+                      video.authors[0].verified == true
+                        ? `<img src="https://img.icons8.com/?size=100&id=SRJUuaAShjVD&format=png&color=000000"" class="w-4 h-4">`
+                        : ``
+                    }
                 </div>
                 <h6 class="text-sm text-[#171717B2]">${video.others.views}</h6>
             </div>
         </div>
+        <button onclick="videoDetails('${
+          video.video_id
+        }')" class="btn btn-block">Show Details</button>
     `;
 
     videoCard.classList.add("rounded-sm", "bg-slate-200");
